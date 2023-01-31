4e67d615fd16058b51c5888b7dcd48fb163ba450
Martin Gonzalez <gonzalez.martin.mag@gmail.com>
2023-01-31 11:59:39 -0300

    Adding prop types to some Components

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react"
+import { useState } from "react";
+import PropTypes from 'prop-types'
 
 export const AddCategory = ({ onNewCategory }) => {
 
-  const [inputValue, setInputValue] = useState('One Punch');
+  const [inputValue, setInputValue] = useState('');
 
   const onInputChange = (e) => {
     setInputValue(e.target.value);
@@ -19,7 +20,7 @@ export const AddCategory = ({ onNewCategory }) => {
   }
 
   return (
-    <form onSubmit={ onSubmit }>
+    <form onSubmit={ onSubmit } aria-label="form">
       <input 
         type="text"
         placeholder="Buscar gifs"
@@ -28,4 +29,8 @@ export const AddCategory = ({ onNewCategory }) => {
       />
     </form>
   )
+}
+
+AddCategory.propTypes = {
+  onNewCategory: PropTypes.func.isRequired
 }
